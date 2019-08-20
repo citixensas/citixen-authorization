e77dbd03f92f8f1d0995041e119992c3dd526602
danor.dcor@gmail.com <danor3122406532>
2019-08-20 14:05:13 -0500

    Removing urls

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import url, include
-
-
-urlpatterns = [
-    url(r'^', include(('companies.urls', 'companies'), namespace='companies')),
-]
+urlpatterns = []
