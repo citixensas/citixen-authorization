13b874769beffd1ae82d22aef51b0d87684693ce
Oscar Pérez <omiguelperez@outlook.com>
2019-08-23 16:35:55 -0500

    Add unique id for users

--- corexen/users/models.py
+++ corexen/users/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from uuid import uuid4
 
 from corexen.companies.models import Headquarter
 from corexen.utils.models import CitixenModel
 
 
 class User(CitixenModel, AbstractUser):
+    uuid = models.UUIDField(default=uuid4, unique=True)
     user_permissions = models.ManyToManyField('auth.Permission', through='users.UserPermission')
 
 
