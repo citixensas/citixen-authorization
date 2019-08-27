18c3a8a9943445f6bbf2d3a2724d8f6b8fd070fa
danor.dcor@gmail.com <danor3122406532>
2019-08-27 09:25:24 -0500

    Test for middleware with invalid token

--- tests/test_users/test_middleware.py
+++ tests/test_users/test_middleware.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from rest_framework.response import Response
 from rest_framework.test import APITestCase
@@ -12,17 +13,29 @@ class PostView(APIView):
         return Response(data=request.data, status=200)
 
 
+class PostWithAuthView(LoginRequiredMixin, PostView):
+    pass
+
+
 urlpatterns = [
     url(r'^post$', PostView.as_view()),
+    url(r'^post_login$', PostWithAuthView.as_view()),
 ]
 
 
 class TestMiddleware(CitixenAPITestCase):
 
-    def test_middleware_can_access_request_post_when_processing_response(self):
+    def test_can_access_request_post_without_login_required(self):
         response = self.client.post('/post', {'foo': 'bar'})
         self.response_200(response)
         response = self.client.post('/post', {'foo': 'bar'}, format='json')
-        assert response.status_code == 200
         self.response_200(response)
         self.assertEquals(response.data, {'foo': 'bar'})
+
+    def test_not_can_access_request_post_with_invalid_token(self):
+        token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIi' \
+                'wiZXhwIjoxNTYzODAzMDQ5LCJqdGkiOiI0NTU4M2FiY2JjZTY0N2MzOWUyNDgwYzg3Z' \
+                'WRjNDc3MyIsInVzZXJfaWQiOjF9.VJXPtY3vh1-2EdPMORL_b9aMPpE7CQveg6akuWEwmmM'
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer %s' % token)
+        response = self.client.post('/post_login', {'foo': 'bar'}, format='json')
+        assert response.status_code == 302
