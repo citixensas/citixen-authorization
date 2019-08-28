4f59a4b8d5c8172d17ea3979f34e389c038c4288
danor.dcor@gmail.com <danor3122406532>
2019-08-28 16:34:21 -0500

    Create an app_user if it does not exist when processing authentication

--- tests/test_users/test_middleware.py
+++ tests/test_users/test_middleware.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from corexen.users.authentication import CitixenAuthentication
+from corexen.users.models import AppUser
 from corexen.utils.testing import CitixenAPITestCase
 from tests.constants import EXAMPLE_TOKEN_VALID, EXAMPLE_TOKEN_INVALID
 
@@ -17,9 +19,15 @@ class PostWithAuthView(PostView):
     permission_classes = (IsAuthenticated,)
 
 
+class PostWithAppUser(PostWithAuthView):
+    def post(self, request):
+        return Response(data=request.user.app_user.id, status=200)
+
+
 urlpatterns = [
     url(r'^post$', PostView.as_view()),
     url(r'^post_login$', PostWithAuthView.as_view()),
+    url(r'^post_app_user$', PostWithAppUser.as_view()),
 ]
 
 
@@ -40,3 +48,12 @@ class TestMiddlewareTestCase(CitixenAPITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer %s' % EXAMPLE_TOKEN_VALID)
         response = self.client.post('/post_login', {'foo': 'bar'}, format='json')
         self.response_200(response)
+
+    def test_can_access_app_user_from_request(self):
+        token = CitixenAuthentication().get_validated_token(EXAMPLE_TOKEN_VALID)
+        app_user = AppUser.objects.create(uuid=token.payload['uuid'])
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer %s' % EXAMPLE_TOKEN_VALID)
+        response = self.client.post('/post_app_user', {'foo': 'bar'}, format='json')
+        self.response_200(response)
+        self.assertEquals(response.data, app_user.pk)
+        self.assertEquals(AppUser.objects.count(), 1)
