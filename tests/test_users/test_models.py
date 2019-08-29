8621a4b932bfbbaf60878013b8e69eddcb45cd69
Oscar Pérez <omiguelperez@outlook.com>
2019-08-29 12:46:35 -0500

    Fix broken tests after merge from master branch

--- tests/test_users/test_models.py
+++ tests/test_users/test_models.py
@@ -15,8 +15,8 @@ class UserModelTestCase(CitixenTestCase):
     def setUp(self):
         self.user = self.make_user()
         self.app_user = AppUser.objects.create(uuid=self.user.uuid)
-        self.company = CompanyFactory(created_by=uuid4())
-        self.headquarter = HeadquarterFactory(company=self.company, created_by=uuid4())
+        self.company = CompanyFactory(created_by=self.app_user)
+        self.headquarter = HeadquarterFactory(company=self.company, created_by=self.app_user)
 
     def test_should_add_permissions_to_user(self):
         self.assertEquals(self.app_user.user_permissions.count(), 0)
@@ -38,7 +38,7 @@ class UserModelTestCase(CitixenTestCase):
         self.assertTrue(self.app_user.has_perm(perm_codename))
 
     def test_should_user_has_not_permission_in_another_headquarter_in_same_company(self):
-        headquarter = HeadquarterFactory(company=self.company, created_by=uuid4())
+        headquarter = HeadquarterFactory(company=self.company, created_by=self.app_user)
         self.assertEquals(self.app_user.user_permissions.count(), 0)
         perm_pks = Permission.objects.all().values_list('codename', flat=True)
         self._add_user_permissions(perms=perm_pks, user=self.app_user,
@@ -49,8 +49,8 @@ class UserModelTestCase(CitixenTestCase):
         self.assertFalse(self.app_user.has_perm(perm_codename))
 
     def test_should_user_has_not_permission_in_another_headquarter_in_other_company(self):
-        company = CompanyFactory(created_by=uuid4())
-        headquarter = HeadquarterFactory(company=company, created_by=uuid4())
+        company = CompanyFactory(created_by=self.app_user)
+        headquarter = HeadquarterFactory(company=company, created_by=self.app_user)
         self.assertEquals(self.app_user.user_permissions.count(), 0)
         perm_pks = Permission.objects.all().values_list('codename', flat=True)
         self._add_user_permissions(perms=perm_pks, user=self.app_user, headquarter=self.headquarter)
