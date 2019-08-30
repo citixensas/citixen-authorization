f7a56fed3d41bb3f4acf779e44b42ffaa6803112
Jose Amaya <eljoseamaya@hotmail.com>
2019-08-30 13:07:50 -0500

    Fixed import of models in company serializers

--- corexen/companies/serializers.py
+++ corexen/companies/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from corexen.companies import Company, Headquarter
+from corexen.companies.models import Company, Headquarter
 
 
 class CompanyModelSerializer(serializers.ModelSerializer):
