0d7fd670dc7ee486eb82ecb7b2cc5cb5f29dd364
Oscar Pérez <omiguelperez@outlook.com>
2019-08-30 13:09:00 -0500

    Fix factories

--- corexen/utils/factories.py
+++ corexen/utils/factories.py
@@ -8,22 +8,22 @@ fake = Faker()
 
 
 class CompanyFactory(DjangoModelFactory):
-    class Meta:
-        model = Company
-
     nit = fake.numerify()
     name = factory.Sequence(lambda n: f'company_{n}')
     email = fake.email()
     country = fake.country()
     image_url = fake.url()
-​
+
+    class Meta:
+        model = Company
+
 
 class HeadquarterFactory(DjangoModelFactory):
-    class Meta:
-        model = Headquarter
-​
     name = factory.Sequence(lambda n: f'headquarter_{n}')
     image_url = fake.url()
     address = fake.address()
     city = fake.city()
     country = fake.country()
+
+    class Meta:
+        model = Headquarter
