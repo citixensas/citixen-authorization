0ae4e25b13eddea8047a238f36ef5502645d3c75
Oscar Pérez <omiguelperez@outlook.com>
2019-08-30 20:14:46 -0500

    Handle error mapping user in middleware

--- corexen/users/authentication.py
+++ corexen/users/authentication.py
@@ -1,15 +1,19 @@
-from corexen.users.models import User, AppUser
 from django.utils.translation import ugettext_lazy as _
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken
+
+from corexen.users.models import User, AppUser
 
 
 class CitixenAuthentication(JWTAuthentication):
     def get_user(self, validated_token):
         try:
             data = validated_token.payload
-            user = User(first_name=data['first_name'],  last_name=data['last_name'],
-                        email=data['email'], username=data['username'], uuid=data['uuid'])
+            user = User(
+                first_name=data.get('first_name', ''), last_name=data.get('last_name', ''),
+                email=data.get('email', ''), username=data.get('username', ''),
+                uuid=data.get('uuid', None)
+            )
             app_user, created = AppUser.objects.get_or_create(uuid=data['uuid'])
             user.app_user = app_user
             return user
