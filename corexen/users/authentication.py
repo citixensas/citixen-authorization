5ab5718d5822157c0646bc9b8e123f35187e3ade
Oscar Pérez <omiguelperez@outlook.com>
2019-09-02 10:58:17 -0500

    Fixing remote authentication

--- corexen/users/authentication.py
+++ corexen/users/authentication.py
@@ -9,14 +9,19 @@ class CitixenAuthentication(JWTAuthentication):
     def get_user(self, validated_token):
         try:
             data = validated_token.payload
-            user = User(
-                first_name=data.get('first_name', ''), last_name=data.get('last_name', ''),
-                email=data.get('email', ''), username=data.get('username', ''),
-                uuid=data.get('uuid', None)
-            )
+
+            # Get or create user
+            user, created = User.objects.get_or_create(uuid=data.get('uuid', None))
+            user.first_name = data.get('first_name', user.first_name)
+            user.last_name = data.get('last_name', user.last_name)
+            user.email = data.get('email', user.email)
+            user.username = data.get('username', user.username)
+
+            # Get or create app user and assign user
             app_user, created = AppUser.objects.get_or_create(uuid=data['uuid'])
             user.app_user = app_user
             user.save()
+
             return user
         except KeyError:
             raise InvalidToken(_('Token contained no recognizable user identification'))
