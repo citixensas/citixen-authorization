54e532222f9b6979dc52893097a5e2d4190a0690
danor.dcor@gmail.com <danor3122406532>
2019-09-09 12:06:47 -0500

    Bug fix for permission test

--- corexen/users/models.py
+++ corexen/users/models.py
@@ -1,12 +1,9 @@
-import unicodedata
 from uuid import uuid4
 
 from django.conf import settings
 from django.contrib import auth
-from django.contrib.auth import password_validation
-from django.contrib.auth.hashers import make_password, check_password, is_password_usable
 from django.contrib.auth.models import UserManager, \
-    _user_has_module_perms, _user_has_perm, _user_get_all_permissions, Permission, Group, AbstractUser
+    _user_has_module_perms, _user_has_perm, _user_get_all_permissions, Permission, Group, AbstractBaseUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.mail import send_mail
 from django.db import models
@@ -18,101 +15,72 @@ from corexen.companies.models import Headquarter
 from corexen.utils.models import CitixenModel
 
 
-class AbstractBaseUser(models.Model):
-    password = models.CharField(_('password'), max_length=128)
-    last_login = models.DateTimeField(_('last login'), blank=True, null=True)
+class AbstractUser(AbstractBaseUser):
+    """
+        An abstract base class implementing a fully featured User model with
+        admin-compliant permissions.
 
-    is_active = True
+        Username and password are required. Other fields are optional.
+        """
+    username_validator = UnicodeUsernameValidator()
 
-    REQUIRED_FIELDS = []
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        unique=True,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    email = models.EmailField(_('email address'), blank=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
-    # Stores the raw password if set_password() is called so that it can
-    # be passed to password_changed() after the model is saved.
-    _password = None
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
     class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
         abstract = True
 
-    def __str__(self):
-        return self.get_username()
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        if self._password is not None:
-            password_validation.password_changed(self._password, self)
-            self._password = None
-
-    def get_username(self):
-        """Return the username for this User."""
-        return getattr(self, self.USERNAME_FIELD)
-
     def clean(self):
-        setattr(self, self.USERNAME_FIELD, self.normalize_username(self.get_username()))
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
 
-    def natural_key(self):
-        return self.get_username(),
-
-    @property
-    def is_anonymous(self):
+    def get_full_name(self):
         """
-        Always return False. This is a way of comparing User objects to
-        anonymous users.
+        Return the first_name plus the last_name, with a space in between.
         """
-        return False
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
 
-    @property
-    def is_authenticated(self):
-        """
-        Always return True. This is a way to tell if the user has been
-        authenticated in templates.
-        """
-        return True
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
 
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-        self._password = raw_password
-
-    def check_password(self, raw_password):
-        """
-        Return a boolean of whether the raw_password was correct. Handles
-        hashing formats behind the scenes.
-        """
-
-        def setter(raw_password):
-            self.set_password(raw_password)
-            # Password hash upgrades shouldn't be considered password changes.
-            self._password = None
-            self.save(update_fields=["password"])
-
-        return check_password(raw_password, self.password, setter)
-
-    def set_unusable_password(self):
-        # Set a value that will never be a valid hash
-        self.password = make_password(None)
-
-    def has_usable_password(self):
-        """
-        Return False if set_unusable_password() has been called for this user.
-        """
-        return is_password_usable(self.password)
-
-    def get_session_auth_hash(self):
-        """
-        Return an HMAC of the password field.
-        """
-        key_salt = "django.contrib.auth.models.AbstractBaseUser.get_session_auth_hash"
-        return salted_hmac(key_salt, self.password).hexdigest()
-
-    @classmethod
-    def get_email_field_name(cls):
-        try:
-            return cls.EMAIL_FIELD
-        except AttributeError:
-            return 'email'
-
-    @classmethod
-    def normalize_username(cls, username):
-        return unicodedata.normalize('NFKC', username) if isinstance(username, str) else username
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
 
 
 class PermissionsMixin(models.Model):
@@ -276,7 +244,7 @@ class UserPermission(CitixenModel):
     """
     This model contains each permission for user in specific headquarter.
     """
-    user = models.ForeignKey('users.user', on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     permission = models.ForeignKey('auth.Permission', on_delete=models.DO_NOTHING)
 
     headquarter = models.ForeignKey(Headquarter, on_delete=models.DO_NOTHING)
