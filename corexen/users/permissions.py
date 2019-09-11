7ed1edb7afe8967540df52939616bf2ad5e5520e
Oscar Pérez <mr.omiguelperez@gmail.com>
2019-09-11 10:48:15 -0500

    Remove app user from drf permission

--- corexen/users/permissions.py
+++ corexen/users/permissions.py
@@ -35,8 +35,7 @@ class UserHeadquarterPermissions(DjangoModelPermissions):
         return [perm % kwargs for perm in self.perms_map[method]]
 
     def has_permission(self, request, view):
-        app_user = getattr(request.user, 'app_user', None)
-        profile = getattr(app_user, 'profile', None) if app_user else None
+        profile = getattr(request.user, 'profile', None)
 
         # Users without headquarter will be restricted with another logic.
         if profile and not profile.has_headquarter() or request.user.is_superuser:
